51c6a3244772f42d7e801622ebbf52c74c8ebcf4
ThienDinh <hellow12343@gmail.com>
2024-08-07 14:10:33 +0700

    Hoan thanh chuc nang tim x tren mang a

--- Bai1.cpp
+++ Bai1.cpp
@@ -8,12 +8,34 @@ int maxEven(int a[], int n);
 int sumOdd(int a[], int n);
 void printOddIndexElements(int a[], int n);
 //=====================Xu ly ham========================
-int sumEven(int a[], int n) {
-	if (n <= 0) {
+int sumEven(int a[], int n) 
+{
+	if (n <= 0) 
+	{
 		return 0;
 	}
 	int sum = (a[n - 1] % 2 == 0) ? a[n - 1] : 0;
 	return sum + sumEven(a, n - 1);
+}
+
+bool binarySearch(int a[], int x, int left, int right) 
+{
+	if (left > right) 
+	{
+		return false;
+	}
+	int mid = left + (right - left) / 2;
+	if (a[mid] == x)
+	{
+		return true;
+	}
+	else if (a[mid] > x) 
+	{
+		return binarySearch(a, x, left, mid - 1);
+	}
+	else {
+		return binarySearch(a, x, mid + 1, right);
+	}
 }
 
 
@@ -25,21 +47,32 @@ int main()
 	int size = sizeof(arr) / sizeof(arr[0]);
 
 	int chon;
-	printf("Moi nhap chuc nang \n ");
+	printf("Moi nhap chuc nang \n");
 	printf("1. Tinh tong chan cua a: \n");
+	printf("2. Tim x tren a dua vao tim kiem nhi phan theo de quy");
 	scanf("%d", &chon);
 
 	switch (chon)
 	{
 	case 1:
-		printf("Sum of even numbers: %d\n", sumEven(arr, size));
+		printf("Tong chan la: %d\n", sumEven(arr, size));
+		break;
+	case 2:
+	{
+			  int x;
+			  printf("Nhap vao so muon tim: ");
+			  scanf("%d", &x);
+			  bool found = binarySearch(arr, x, 0, size - 1);
+			  printf("so %d %s tim thay trong mang.\n", x, found ? "duoc" : "khong duoc");
+	}
 		break;
 	
 	default:
-		printf("Invalid operation.\n");
+		printf("moi nhap lai.\n");
 		break;
 	}
-	printf("Press any key to continue . . .");
+
+	printf("Nhan nut bat ky de tiep tuc . . .");
 	getchar(); 
 	getchar();
 	return 0;
